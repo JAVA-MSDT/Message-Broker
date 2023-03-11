9f23a099779b7618116ad5b7063ffdc6a3aa0adc
Ahmed Samy <serenitydiver@hotmail.com>
2023-03-11 10:12:46 +0100

    Replacing the old manual structure with maven multi mnodule project

--- client/src/main/java/org/client/MainClient.java
+++ client/src/main/java/org/client/MainClient.java
@@ -5,8 +5,10 @@ import io.grpc.ManagedChannelBuilder;
 
 public class MainClient {
 
+    public static final String SERVER_URL = "localhost:8080";
+
     public static void main(String[] args) {
-        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(SERVER_URL)
                 .usePlaintext()
                 .build();
         var client = new MessageClient(channel);
