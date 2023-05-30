749311e071ea33f2a845156b6261ff678a19a6f2
Ahmed Samy <Ahmed_Samy_Bakry_Mahmoud@Epam.Com>
2023-05-30 14:11:19 +0200

    Initializing Kafka producer module

---
+++ Kafka/Linkedin/Kafka-Basics/producer/src/main/java/org/javamsdt/producer/Producer.java
@@ -0,0 +1,59 @@
+package org.javamsdt.producer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Producer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Producer.class.getSimpleName());
+
+    public static void main(String[] args) {
+        LOGGER.info("Producer Started .....");
+
+        // Producer Properties
+        Properties properties = getKafkaProperties();
+        properties.put("key.serializer", StringSerializer.class.getName());
+        properties.put("value.serializer", StringSerializer.class.getName());
+
+        // Producer
+        KafkaProducer<String, String> producer = getKafkaProducer(properties);
+
+        // Producer Record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "helloWorld");
+
+        // Send data
+        producer.send(producerRecord);
+
+        // flush & close the producer
+        producer.flush();
+        producer.close();
+    }
+
+    private static Properties getKafkaProperties() {
+        Properties properties = new Properties();
+        try (InputStream input = Producer.class.getClassLoader().getResourceAsStream("kafka.properties")) {
+
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+            }
+            properties.load(input);
+            LOGGER.info("Bootstrap Server: " + properties.getProperty("bootstrap.servers"));
+
+        } catch (IOException ex) {
+            LOGGER.error("Error Loading Kafka Properties....");
+            ex.printStackTrace();
+        }
+        return properties;
+    }
+
+    private static KafkaProducer<String, String> getKafkaProducer(Properties kafkaProperties) {
+        return new KafkaProducer<>(kafkaProperties);
+    }
+}
